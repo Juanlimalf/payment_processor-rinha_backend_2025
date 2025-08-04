a4e0446d113d4b789ef36acf596debb9163b1870
Juan Lima <juanlimalf@gmail.com>
2025-08-04 00:14:41 -0300

    CHORE: Adiciona log_job_description desativado ao trabalhador RQ

--- worker/main.py
+++ worker/main.py
@@ -4,8 +4,7 @@ from rq import SimpleWorker
 from config import settings
 
 redis_conn = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
-worker = SimpleWorker(["worker"], connection=redis_conn)
-
+worker = SimpleWorker(["worker"], connection=redis_conn, log_job_description=False)
 
 if __name__ == "__main__":
     worker.work()
