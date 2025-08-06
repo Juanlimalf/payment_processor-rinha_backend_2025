9c2061e5bffa46c97a6ad7a3445acbc3ff042b88
Juan Lima <juanlimalf@gmail.com>
2025-08-06 17:37:00 -0300

    CHORE: ativa o thread3 no processamento do worker

--- worker/main.py
+++ worker/main.py
@@ -20,12 +20,12 @@ def main(thread_id: int):
 if __name__ == "__main__":
     thread1 = threading.Thread(target=main, args=(1,), name="WorkerThread-1")
     thread2 = threading.Thread(target=main, args=(2,), name="WorkerThread-2")
-    # thread3 = threading.Thread(target=main, args=(3,), name="WorkerThread-3")
+    thread3 = threading.Thread(target=main, args=(3,), name="WorkerThread-3")
 
     thread1.start()
     thread2.start()
-    # thread3.start()
+    thread3.start()
 
     thread1.join()
     thread2.join()
-    # thread3.join()
+    thread3.join()
